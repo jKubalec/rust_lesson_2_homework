196815ec8c0aa4148b2d9e8abec3509bf0928602
jan.kubalec <jan.kubalec@t-mobile.nl>
2023-10-17 17:48:09 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::env;
+use slug::slugify;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args[1].is_empty() {
+        println!("No argument given. Exiting...");
+        return;
+    }
+
+    let transformation = &args[1];
+    println!("Let's {} some input!", transformation);
+
+    let mut raw_string = String::new();
+
+    match io::stdin().read_line(&mut raw_string) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Failed to read {}", e);
+            return;
+        }
+    }
+
+    let result = match transformation.as_str() {
+        "lowercase" => {raw_string.to_lowercase()}
+        "uppercase" => {raw_string.to_uppercase()}
+        "no-spaces" => {raw_string.replace(" ", "")}
+        "slugify"   => {slugify(raw_string)}
+        _ => {String::from("Unknown operation. Original string: ".to_owned() + &raw_string)}
+    };
+
+    println!("Result:\n{}", result);
+}
